2e73eabe9756989e4f72a82c5caf3e3c8a44b3b8
Leandro <contato.leandroelias@gmail.com>
2025-08-25 18:55:54 -0300

    atualizacao desafio nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,34 +3,56 @@
 #include <math.h>
 #include <stdlib.h>
 
+#define LINHAS 10
+#define COLUNAS 10
+
+
 int main(){
 
-    int navio1[3] = {3, 3, 3};
+    int navio[3] = {3, 3, 3};
 
-    int navio2[3] = {3, 3, 3};
 
-    int tabuleiro[10][10];
+    int tabuleiro[LINHAS][COLUNAS];
 
 
     // INICIANDO  O TABULEIRO
-    for (int i = 0 ; i < 10; i++){
-        for (int j = 0; j < 10 ; j++){
+    for (int i = 0 ; i < LINHAS; i++){
+        for (int j = 0; j < COLUNAS ; j++){
             tabuleiro[i][j] = 0;
         }
     }
 
 
-    // POSICIONANDO O NAVIO 1 NA HORIZONTAL (COLUNA 0, CASAS 3 A 5)
+    // POSICIONANDO O NAVIO 1 NA HORIZONTAL (LINHA 1, CASAS 3 A 5)
 
     for (int j = 0; j < 3; j++){
-        tabuleiro[0][3 + j] = navio1[j];
+        tabuleiro[0][3 + j] = navio[j];
     }
 
-    // POSICIONANDO O NAVIO 2 NA VERTICAL (COLUNAS 5, 6, 7, CASAS 0)
+    // POSICIONANDO O NAVIO 2 NA VERTICAL (LINHAS 5, 6, 7, CASAS 0)
 
     for (int i = 0; i < 3; i++){
-        tabuleiro[i + 4][0] = navio2[i];
+        tabuleiro[i + 4][0] = navio[i];
     }
+
+
+    // POSICIONANDO UM NAVIO NA DIAGONAL (LINHAS 1, 2, 3)
+
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 1; j++){
+                tabuleiro[i][i] = navio[j];
+
+        }
+    }
+
+    // POSICIONANDO O SEGUNDO NAVIO NA DIAGONAL (LINHAS 10, 9, 8)
+
+    for (int i = 9 ; i > 6; i--){
+        for (int j = 0; j < 1; j++){
+            tabuleiro[i][i] = navio[j];
+        }
+    }
+
 
     //MOSTRANDO NA TELA
     for (int i = 0; i < 10 ; i++){
